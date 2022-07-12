faf8d5c0cf3b44208a370ef1dd0a2bb2e6c31754
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-07-13 00:44:01 +0100

    fixed graphql ws server
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
 import "reflect-metadata";
-import { ApolloServer } from "apollo-server-express";
-import express from "express";
 import { buildSchema } from "type-graphql";
-import cors from "cors";
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
 import dotenv from "dotenv";
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 import { ChatResolver } from "./resolvers/ResolverChat";
-import { createServer } from "http";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import { WebSocketServer } from "ws";
-import { useServer } from "graphql-ws/lib/use/ws";
 
 dotenv.config();
 
 const main = async () => {
-  const app = express();
-  const httpServer = createServer(app);
-  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-
   const schema = await buildSchema({
     resolvers: [ChatResolver],
     validate: false,
   });
+
+  // Required logic for integrating with Express
+  const app = express();
+  const httpServer = http.createServer(app);
 
   // Creating the WebSocket server
   const wsServer = new WebSocketServer({
@@ -28,16 +27,19 @@ const main = async () => {
     server: httpServer,
     // Pass a different path here if your ApolloServer serves at
     // a different path.
-    path: "/subscription",
+    path: '/graphql',
   });
 
   // Hand in the schema we just created and have the
   // WebSocketServer start listening.
   const serverCleanup = useServer({ schema }, wsServer);
 
-  const apolloServer = new ApolloServer({
-    schema: schema,
-    plugins: [
+  // Same ApolloServer initialization as before, plus the drain plugin.
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    cache: 'bounded',
+     plugins: [
       // Proper shutdown for the HTTP server.
       ApolloServerPluginDrainHttpServer({ httpServer }),
 
@@ -54,18 +56,20 @@ const main = async () => {
     ],
   });
 
-  apolloServer.start().then(() => {
-    apolloServer.applyMiddleware({
-      app,
-      cors: false,
-    });
+  // More required logic for integrating with Express
+  await server.start();
+  server.applyMiddleware({
+    app,
 
-    app.listen(process.env.PORT, () => {
-      console.log(
-        `Server ready at http://localhost:${process.env.PORT}${apolloServer.graphqlPath}`
-      );
-    });
+    // By default, apollo-server hosts its GraphQL endpoint at the
+    // server root. However, *other* Apollo Server packages host it at
+    // /graphql. Optionally provide this to match apollo-server.
+    path: '/graphql'
   });
+
+  // Modified server startup
+  await new Promise<void>(resolve => httpServer.listen({ port: process.env.PORT }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`);
 };
 
 main().catch((err) => {
--- src/resolvers/ResolverChat.ts
+++ src/resolvers/ResolverChat.ts
@@ -10,7 +10,7 @@ import {
 } from "type-graphql";
 import { Chat } from "../entities/Chat";
 
-const chats: Chat[] = [];
+let chats: Chat[] = [];
 const new_mesage = "NEW_MESSAGE_CHANNEL";
 const new_chat = "NEW_CHAT_CHANNEL";
 
@@ -25,12 +25,11 @@ export class ChatResolver {
   async createChat(
     @PubSub() pubSub: PubSubEngine,
     @Arg("name") name: string,
-    @Arg("desciption") description: string
+    @Arg("description") description: string
   ): Promise<Chat> {
     const chat = { id: chats.length + 1, name, description };
     chats.push(chat);
-    const payload = chat;
-    await pubSub.publish(new_chat, payload);
+    await pubSub.publish(new_chat, undefined);
     return chat;
   }
 
@@ -40,7 +39,7 @@ export class ChatResolver {
   }
 
   @Subscription({ topics: new_chat })
-  chatCreated(@Root() { id, name, description }: Chat): Chat {
-    return { id, name, description };
+  chatCreated(): Chat {
+    return chats[chats.length - 1];
   }
 }
